b8bed5e3b263048de12f206d470af9750c377fd9
Panagiotis Plytas <panagiotis.plytas@gmail.com>
2018-12-02 16:16:10 +0200

    Add cards

--- index.php
+++ index.php
@@ -35,7 +35,102 @@
 				<span class="navbar-text white-text">Εισοδος</span>
 			</div>
 		</nav>
+		<div class="container text-center">
+			<div class="row my-5">
+			<!-- Card -->
+				<div class="card col-xs mx-1 w-25 h-25">
 
+				  <!-- Card content -->
+				  <div class="card-body light-green">
+
+				    <!-- Title -->
+				    <h4 class="card-title"><a>ΦΟΙΤΗΤΕΣ</a></h4>
+				    <!-- Text -->
+				    <p class="card-text"></p>
+
+				  </div>
+
+				</div>
+				<!-- Card -->
+				<!-- Card -->
+				<div class="card col-xs mx-1 w-25 h-25">
+
+				  <!-- Card content -->
+				  <div class="card-body yellow">
+
+				    <!-- Title -->
+				    <h4 class="card-title"><a>ΓΡΑΜΜΑΤΕΙΑ</a></h4>
+				    <!-- Text -->
+				    <p class="card-text"></p>
+
+				  </div>
+
+				</div>
+				<!-- Card -->
+				<!-- Card -->
+				<div class="card col-xs mx-1 w-25 h-25">
+
+				  <!-- Card content -->
+				  <div class="card-body pink">
+
+				    <!-- Title -->
+				    <h4 class="card-title"><a>ΕΚΔΟΤΕΣ</a></h4>
+				    <!-- Text -->
+				    <p class="card-text"></p>
+
+				  </div>
+
+				</div>
+				<!-- Card -->
+			</div>
+			<div class="row my-5">
+				<!-- Card -->
+				<div class="card col-xs mx-1 w-25 h-25">
+
+				  <!-- Card content -->
+				  <div class="card-body purple">
+
+				    <!-- Title -->
+				    <h4 class="card-title"><a>ΣΗΜΕΙΑ ΔΙΑΝΟΜΗΣ</a></h4>
+				    <!-- Text -->
+				    <p class="card-text"></p>
+
+				  </div>
+
+				</div>
+				<!-- Card -->
+				<!-- Card -->
+				<div class="card col-xs mx-1 w-25 h-25">
+
+				  <!-- Card content -->
+				  <div class="card-body amber">
+
+				    <!-- Title -->
+				    <h4 class="card-title"><a>ΒΙΒΛΙΟΘΗΚΕΣ</a></h4>
+				    <!-- Text -->
+				    <p class="card-text"></p>
+
+				  </div>
+
+				</div>
+				<!-- Card -->
+				<!-- Card -->
+				<div class="card col-xs mx-1 w-25 h-25">
+
+				  <!-- Card content -->
+				  <div class="card-body lime">
+
+				    <!-- Title -->
+				    <h4 class="card-title"><a>ΔΩΡΕΑΝ ΣΗΜΕΙΩΣΕΙΣ/ΔΙΔΑΣΚΟΝΤΕΣ</a></h4>
+				    <!-- Text -->
+				    <p class="card-text"></p>
+
+				  </div>
+
+				</div>
+				<!-- Card -->
+			</div>
+		</div>
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.15/js/mdb.min.js"></script>
