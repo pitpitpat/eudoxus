efb0e937ac6f2e1e6a6b45a10b224d77ce3fd03a
Panagiotis Plytas <panagiotis.plytas@gmail.com>
2018-12-05 00:17:24 +0200

    Add post request example and comments in code

--- frontend/js/test-endpoints.js
+++ frontend/js/test-endpoints.js
@@ -1,13 +1,38 @@
-var xamppDirectory = "/eudoxus";
-var api = "http://localhost" + xamppDirectory + "/backend/api/endpoints";
+var xamppDirectory = "/eudoxus";											// XAMPP htdocs directory name
+var api = "http://localhost" + xamppDirectory + "/backend/api/endpoints";	// Path to api endpoints
 
-
+// GET request
 var endpoint = "/student/read.php";
 var url = api + endpoint;
 $.ajax({
 	type: "GET",
 	url: url,
 	dataType: "json",
+	success: function(result, status, xhr) {
+		console.log(result);
+		console.log(status);
+		console.log(xhr);
+	},
+	error: function(xhr, status, error) {
+		console.log(xhr);
+		console.log(status);
+		console.log(error);
+	}
+});
+
+// POST request
+var endpoint = "/student/read.php";
+var url = api + endpoint;
+var data = {
+	name: "panagiotis",
+	surname: "plytas",
+	age: 21
+};
+$.ajax({
+	type: "POST",
+	url: url,
+	data: data,
+	dataType: "json", 
 	success: function(result, status, xhr) {
 		console.log(result);
 		console.log(status);
