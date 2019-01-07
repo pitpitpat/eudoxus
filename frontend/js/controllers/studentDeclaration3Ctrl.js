2e22dfe90889540f1e4810e795ed43c0cd61ca73
Panagiotis Plytas <panagiotis.plytas@gmail.com>
2019-01-07 21:21:06 +0200

    Add more steps to student declaration

--- frontend/js/controllers/studentDeclaration3Ctrl.js
+++ frontend/js/controllers/studentDeclaration3Ctrl.js
@@ -5,11 +5,20 @@
 
 		$scope.declarationStep = 3;
 
+		$scope.goToDeclaration4 = function() {
+			window.location.href = "#!/student/declaration/final";
+		}
+
 		/* ================= On start ================= */
 
 		console.log("student-3");
 
 		console.log($rootScope.user.declaration);
+
+		for (courseId in $rootScope.user.declaration.books) {
+			var book = $rootScope.user.declaration.books[courseId];
+			book.takeMethod = "store";
+		}
 
 
 	});
